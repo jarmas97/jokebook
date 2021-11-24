7e5ef425f69e9b69736d14766b8f246c39bd3c6f
jarmas97 <jerry.maslowski97@gmail.com>
2021-11-24 10:22:23 +0100

    Initial commit

---
+++ src/test/java/com/github/jarmas97/jokebook/JokebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jarmas97.jokebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
